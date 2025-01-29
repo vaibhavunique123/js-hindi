9c277a7eca1e93489e2aa1e92223d0d4208fa710
Vaibhav Srivastava <94758421+vaibhavunique123@users.noreply.github.com>
2025-01-29 15:06:21 +0000

    datatype

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -4,15 +4,36 @@ const bigNumber=123453565223332n
 // reference non primitive 
 // array object function  
 const hero= ["vaibhav", "jojo","neha"]
-console.log(hero)
+//console.log(hero)
 
 let myObj={
     name:"vaibhav",
     age:24,
 }
-console.log(myObj)
+//console.log(myObj)
 
 const myFunction=function(){
     console.log("vaibhav")
 }
-console.log(myFunction)
+//console.log(myFunction)
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack(primitive) and heap(non-primitive)
+
+let firstvalue="vaibhav 1"
+let secondvalue= firstvalue
+secondvalue="vaibhav 3"
+//console.log(firstvalue)
+//console.log(secondvalue)
+
+let firstobj ={
+    name:"vaibhav",
+    upi:"vaibahv_paytm"
+}
+
+let second_obj= firstobj
+second_obj.name="rakesh"
+
+console.log(firstobj)
+console.log(second_obj)
