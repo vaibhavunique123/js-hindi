e5b4a13341146e23695ced38292c939303dca062
Vaibhav Srivastava <94758421+vaibhavunique123@users.noreply.github.com>
2025-01-29 14:26:58 +0000

    datatypes_summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,18 @@
+const bigNumber=123453565223332n
+//console.log(bigNumber)
+
+// reference non primitive 
+// array object function  
+const hero= ["vaibhav", "jojo","neha"]
+console.log(hero)
+
+let myObj={
+    name:"vaibhav",
+    age:24,
+}
+console.log(myObj)
+
+const myFunction=function(){
+    console.log("vaibhav")
+}
+console.log(myFunction)
