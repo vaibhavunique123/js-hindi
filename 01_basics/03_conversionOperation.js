b89a85842a61b03dd13c18e98b702016bbb86aff
Vaibhav Srivastava <94758421+vaibhavunique123@users.noreply.github.com>
2025-01-29 13:18:24 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let score = null
+//console.log(typeof score);
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber); 
+//console.log(valueInNumber)
+
+let someNumber = 33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber) 
