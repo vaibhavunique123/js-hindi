6a4d387957d9b33649f365d1f28f12589a278319
Vaibhav Srivastava <94758421+vaibhavunique123@users.noreply.github.com>
2025-01-29 15:37:49 +0000

    string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,3 +1,21 @@
 const name="vaibhav"
 const repoCount=50
-console.log(name+repoCount)
+//console.log(name+repoCount)
+
+const gameName= new String("vaibhav")
+
+console.log(gameName[0]);
+console.log(gameName.__proto__)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('b'))
+
+const newString=gameName.substring(0,4)
+console.log(newString);
+
+const anotherString=gameName.slice(-5,3)
+console.log(anotherString);
+
+const url="https://vaibhav.com/vaibhav%20srivastava"
+
+console.log(url.replace('%20','-'))
