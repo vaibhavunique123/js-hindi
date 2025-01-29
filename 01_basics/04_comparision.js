4254292d457ac569c1004506e62fb3f2b80e98c7
Vaibhav Srivastava <94758421+vaibhavunique123@users.noreply.github.com>
2025-01-29 14:27:36 +0000

    datatypes-summary.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,4 +1,18 @@
-console.log("2">1);
-console.log(null>1)
-console.log(null<1)
-console.log(null>=0)
+//console.log("2">1);
+//console.log(null>1)
+//console.log(null<1)
+//console.log(null>=0)
+
+const bigNumber=123453565223332n
+//console.log(bigNumber)
+
+// reference non primitive 
+// array object 
+const hero= ["vaibhav", "jojo","neha"]
+console.log(hero)
+
+let myObj={
+    name:"vaibhav",
+    age:24,
+}
+console.log(myObj)
