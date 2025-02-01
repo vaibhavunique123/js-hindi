7ac8077cd3356b3e39badbf9f05cd3a0f6f33b7e
Vaibhav Srivastava <94758421+vaibhavunique123@users.noreply.github.com>
2025-02-01 19:17:44 +0000

    maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,14 @@
+const score =400
+//console.log(score);
+const balance =new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2))
+console.log(Math)
+console.log(Math.round(4.4))
+console.log(Math.random()*10)
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
